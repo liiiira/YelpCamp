6a80927e2f07ed84bcbdf0b71ffac4946e04e17c
liiiira <benyahia.lyes.00@gmail.com>
2025-08-26 12:40:30 +0100

    add mongoDB

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+async function connectDB(){
+    try{
+        await mongoose.connect("mongodb://127.0.0.1:27017/YelpCamp");
+        console.log("connected to database");
+    }catch(e){
+        console.log("Failed To Connect To Database");
+    }
+}
+
+connectDB();
 
 app.get("/", (req, res) => {
     res.send("Welcome");
