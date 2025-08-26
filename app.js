dfe3a1858fbb1ed9550fd421154989e05fb6ff6c
liiiira <benyahia.lyes.00@gmail.com>
2025-08-26 12:30:11 +0100

    add express and start project

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Welcome");
+})
+
+app.listen(5000, (req, res) => {
+    console.log("We are listening on Port 5000");
+})
